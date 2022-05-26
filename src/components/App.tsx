ed2995e1f44f4e2058031b59fc5c4e3677f45585
koskedk <koskedk@live.com>
2022-05-26 15:23:14 +0300

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './App.css';
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+
+const App=()=> {
+    return (
+        <div>
+            <Header/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Footer=()=>{
+    return (<footer><h4>v1 {process.env.REACT_APP_CRS_VER}</h4></footer>)
+}
+export default Footer;
